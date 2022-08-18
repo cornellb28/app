c44005ea4c44eff6b7222556ccb886b15cc831e8
Cornell Benson <cornellb28@gmail.com>
2022-08-18 16:06:09 -0500

    Update useTracks.ts

--- src/hooks/useTracks.ts
+++ src/hooks/useTracks.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import tracks from "../../data/tracks.json";
+import { v4 as uuidv4 } from "uuid";
 
 function useTracks() {
   const [tracksData, setTrackData] = useState([]);
